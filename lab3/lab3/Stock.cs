397614659eab099b2eb91cfb25794107025fa36c
Cody Gildea <cbgildea@gmail.com>
2014-09-17 22:34:21 -0700

    Mostly done with lab 3

---
+++ lab3/lab3/Stock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Stock
+    {
+        public String name{get; private set;}
+        public int initialValue{get; private set;}
+        public int currentValue{get; private set;}
+        public int maxChange{get; private set;}
+        public int threshold{get; private set;}
+        public int changes { get; private set; }
+        public int difference { get; private set; }
+
+        private Thread thread;
+
+        public Stock(String name, int initialValue, int maxChange, int threshold)
+        {
+            this.name = name;
+            this.initialValue = initialValue;
+            this.maxChange = maxChange;
+            this.threshold = threshold;
+            thread = new Thread(new ThreadStart(Activate));
+            thread.Start();
+        }
+        public void Activate(){
+		    for(;;){
+                Thread.Sleep(500);
+			    changeStockValue();
+		    }
+	    }
+
+	    public void changeStockValue(){
+            Random rnd = new Random();
+		    currentValue = initialValue + rnd.Next(-maxChange, maxChange);
+            if (currentValue < 0)
+                currentValue = 0;
+
+            changes += Math.Abs(currentValue - initialValue);
+
+            ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+
+            if (changes >= threshold)
+            {
+                this.difference = this.currentValue - this.initialValue;
+                args.name = this.name;
+                args.initialValue = this.initialValue;
+                args.currentValue = this.currentValue;
+                args.changes = this.changes;
+                args.difference = this.difference;
+                this.initialValue = this.currentValue;
+                OnThresholdReached(args);
+            }
+            else
+            {
+                this.initialValue = this.currentValue;
+            }
+	    }
+
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            ThresholdReachedEventHandler handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event ThresholdReachedEventHandler ThresholdReached;
+
+    }
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public String name { get; set; }
+        public int initialValue { get; set; }
+        public int currentValue { get; set; }
+        public int changes { get; set; }
+        public int difference { get; set; }
+    }
+    public delegate void ThresholdReachedEventHandler(Object sender, ThresholdReachedEventArgs e);
+}
---
+++ lab3/lab3/StockBroker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class StockBroker
+    {
+        public static event EventHandler _stockPrint;
+
+        public String BrokerName { get; private set; }
+        public List<Stock> stocks { get; private set; }
+
+        public StockBroker(String name)
+        {
+            this.BrokerName = name;
+            
+        }
+
+	    public void addStock(Stock s){
+            s.ThresholdReached += s_ThresholdReached; //To Text File
+	    }
+
+        void s_ThresholdReached(Object sender, ThresholdReachedEventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\User\\dev\\cecs475\\lab3\\lab3\\lab3Output.txt", true);
+
+            if (e.difference > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}\n", BrokerName, e.name, e.initialValue, e.currentValue, e.difference, e.changes);
+
+                file.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}\n", BrokerName, e.name, e.initialValue, e.currentValue, e.difference, e.changes);
+
+                file.Close();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}\n", BrokerName, e.name, e.initialValue, e.currentValue, e.difference, e.changes);
+
+                file.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}\n", BrokerName, e.name, e.initialValue, e.currentValue, e.difference, e.changes);
+
+                file.Close();
+            }
+            
+        }
+    }
+}
