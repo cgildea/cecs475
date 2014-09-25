1fbaf32c989c7be443e496e9e6c4fb92ffda9091
Cody Gildea <cbgildea@gmail.com>
2014-09-25 16:06:44 -0700

    Added lock

--- lab3/lab3/FilePrint.cs
+++ lab3/lab3/FilePrint.cs
@@ -9,11 +9,13 @@ namespace lab3
 {
     class FilePrint
     {
-        String path; 
+        String path;
+        private System.Object lockThis = new System.Object();
 
         public FilePrint()
         {
             path = @" C:\\Users\\User\\dev\\cecs475\\lab3\\lab3\\lab3Output.txt";
+
         }
 
         public void PrintToFile(String brokerName, String stockName, int initialValue, int currentValue, int difference, int changes, DateTime timeStamp)
@@ -22,25 +24,28 @@ namespace lab3
             {
                 try
                 {
-                    if (difference > 0)
+                    lock (lockThis)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
-                                difference, changes, timeStamp.ToString());
+                        if (difference > 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
+                                    difference, changes, timeStamp.ToString());
 
-                        print.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
-                                difference, changes, timeStamp.ToString());
-                        print.Close();
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
-                                difference, changes, timeStamp.ToString());
+                            print.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
+                                    difference, changes, timeStamp.ToString());
+                            print.Close();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
+                                    difference, changes, timeStamp.ToString());
 
-                        print.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
-                                difference, changes, timeStamp.ToString());
-                        print.Close();
+                            print.WriteLine("{0,-15}{1,-10}\t{2,-10}{3,-10}{4,-15}{5,-10}{6,-10}\n", brokerName, stockName, initialValue, currentValue,
+                                    difference, changes, timeStamp.ToString());
+                            print.Close();
+                        }
                     }
                 }
                 catch (IOException)
