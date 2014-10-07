9c4dde2888221b4099acb02aa58cab8d04e0b490
Cody Gildea <cbgildea@gmail.com>
2014-10-07 12:30:44 -0700

    Finished lab 4

---
+++ lab4-2/JoinQueries/JoiningTableData.cs
@@ -0,0 +1,105 @@
+﻿// Fig. 22.24: JoiningTableData.cs
+// Using LINQ to perform a join and aggregate data across tables.
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JoinQueries
+{
+   public partial class JoiningTableData : Form
+   {
+      public JoiningTableData()
+      {
+         InitializeComponent();
+      } // end constructor
+
+      private void JoiningTableData_Load( object sender, EventArgs e )
+      {
+         // Entity Framework DBContext
+         BooksExamples.BooksEntities dbcontext =
+            new BooksExamples.BooksEntities();
+
+         // get authors and titles of each book they co-authored
+         var authorsAndTitles =
+            from book in dbcontext.Titles
+            from author in book.Authors
+            orderby book.Title1
+            select new { author.FirstName, author.LastName,
+               book.Title1 };
+
+         outputTextBox.AppendText( "Authors and titles:" );
+
+         // display authors and titles in tabular format
+         foreach ( var element in authorsAndTitles )
+         {
+            outputTextBox.AppendText(
+               String.Format( "\r\n\t{0,-10}\t {1,10} {2}",
+                    element.Title1, element.FirstName, element.LastName  ) );
+         } // end foreach
+
+
+          //Get authors and titles and order by title, author name
+          var authorsAndTitlesOrdered =
+          from book in dbcontext.Titles
+          from author in book.Authors
+          orderby book.Title1, author.LastName, author.FirstName
+          select new { author.FirstName, author.LastName, book.Title1 };
+
+          outputTextBox.AppendText("\r\n\r\nTitles and Authors Ordered:");
+
+          // display authors and titles in tabular format
+          foreach (var element in authorsAndTitlesOrdered)
+          {
+                outputTextBox.AppendText(
+                String.Format("\r\n\t{0,-10}\t {1,10} {2}",
+                         element.Title1, element.FirstName, element.LastName));
+          } // end foreach
+
+
+         // get authors and titles of each book 
+         //  group by titles, order by titles, author
+          var authorsByTitleOrdered =
+             from book in dbcontext.Titles
+             orderby book.Title1
+             select new
+             {
+                 Name = book.Title1,
+                 Authors =
+                    from author in book.Authors
+                    orderby author.LastName, author.FirstName
+                    select new  { author.LastName, author.FirstName }
+             };
+
+         outputTextBox.AppendText( "\r\n\r\nTitles grouped by author:" );
+
+         // display titles written by each author, grouped by author
+         foreach (var title in authorsByTitleOrdered)
+         {
+            // display author's name
+            outputTextBox.AppendText( "\r\n" + title.Name + ":" );
+
+            // display titles written by that author
+            foreach ( var author in title.Authors )
+            {
+               outputTextBox.AppendText( "\r\n\t\t" + author.FirstName + " " + author.LastName);
+            } // end inner foreach
+         } // end outer foreach
+      } // end method JoiningTableData_Load
+   } // end class JoiningTableData
+} // end namespace JoinQueries
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Invoice[] invoices = 
+           {    
+                new Invoice(83, "Electric sander", 7, 57.98m),
+                new Invoice(24, "Power saw", 18, 99.99m),
+                new Invoice(7, "Sledge hammer", 11, 21.50m),
+                new Invoice(77, "Hammer", 76, 11.99m),
+                new Invoice(39, "Lawn mower", 3, 79.50m),
+                new Invoice(68, "Screwdriver", 106, 6.99m),
+                new Invoice(56, "Jig saw", 21, 11.00m),
+                new Invoice(3, "Wrench", 34, 7.50m)
+           };
+
+           var descriptions = from i in invoices
+                             orderby i.PartDescription
+                             select new { i.PartNumber, i.PartDescription, i.Quantity, i.Price };
+
+           Console.WriteLine("Ordered by Description:");
+           foreach (var invoice in descriptions)
+           {
+               Console.WriteLine("{0, -10}{1, -20}{2, -10}{3, -10}", invoice.PartNumber,
+                   invoice.PartDescription, invoice.Quantity, invoice.Price);
+           }
+           Console.WriteLine("\n\nOrdered by Price:");
+           var prices = from i in invoices
+                             orderby i.Price
+                             select new { i.PartNumber, i.PartDescription, i.Quantity, i.Price };
+
+           foreach (var invoice in prices)
+           {
+               Console.WriteLine("{0, -10}{1, -20}{2, -10}{3, -10}", invoice.PartNumber,
+                   invoice.PartDescription, invoice.Quantity, invoice.Price);
+           }
+
+           Console.WriteLine("\n\nDescription and Quantity; Ordered by Quantity:");
+           var descQuantity = from i in invoices
+                        orderby i.Quantity
+                        select new {i.PartDescription, i.Quantity};
+
+           foreach (var invoice in descQuantity)
+           {
+               Console.WriteLine("{0, -20}{1, -10}",
+                   invoice.PartDescription, invoice.Quantity);
+           }
+
+           Console.WriteLine("\n\nInvoice Total; Ordered by Value:");
+           var invoiceTotal = from i in invoices
+                              let total = i.Price * i.Quantity
+                              orderby total
+                              select new { i.PartDescription, i.Quantity, total };
+
+           foreach (var invoice in invoiceTotal)
+           {
+               Console.WriteLine("{0, -20}{1, -10}{2, -10}",
+                   invoice.PartDescription, invoice.Quantity, invoice.total);
+           }
+
+           var invoiceTotalRange = from t in invoiceTotal
+                                   where t.total >= 200 && t.total <= 500
+                                   select new { t.PartDescription, t.Quantity, t.total };
+
+           Console.WriteLine("\n\nInvoice Total Range; Ordered by Value:");
+
+           foreach (var invoice in invoiceTotalRange)
+           {
+               Console.WriteLine("{0, -20}{1, -10}{2, -10}",
+                   invoice.PartDescription, invoice.Quantity, invoice.total);
+           }
+        }
+    }
+}
